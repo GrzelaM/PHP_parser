33cd8aabe1998536cda0ce621d9cb40ddbde220f
Mateusz Grzela <grzela6@o2.pl>
2020-02-26 16:07:56 +0100

    Parsowanie zbiorowe

--- index.php
+++ index.php
@@ -5,38 +5,22 @@
 </head>
 <body>
 <?php
-	$file = "Input_file/inp3.xml";
-	//Obiekt do parsowania XML
+	for($it = 1; $it < 154; $it++){
+		$file = "Inp/1 (".$it.").xml";
+		//Obiekt do parsowania XML
 	$dom = new DOMDocument();
 	//Wczytaj XML
 	$dom->load($file);
-	$myfile = fopen("Output_file/outp3.txt", "w");
+	$myfile = fopen("Output_file/1 (".$it.").txt", "w");
 
 	// tekst, który nas interesuje znajduję się pomiędzy znacznikami body
 	$article = $dom->getElementsByTagName("body")->item(0);
 	$counter = 0; // licznik cytatów
 	foreach ($article->getElementsByTagName("xref") as $xref) {
 		if ($xref->getAttribute("ref-type") == "bibr") {
-			//$xref jest cytatem, zastepujemy standardowy cytat napisem: [cytat]
-			//$xref->nodeValue = "[cytat]";
 			$counter++;  // inkrementacja liczby cytatów
 			$text = $xref->nodeValue;
-			// sprawdzamy czy pobrany text to [1] czy 1, ponieważ w jednym przypadku
-			// mamy: [<tag>1<tag>] i zamienienie tego na [cytat] dałoby nam [[cytat]]
-			// mamy też 3 przypadek ...<tag>1<tag>.... 
-			// sprawdzanie odbywa się tak jeżeli mamy [1] to zamieniamy na [cytat]
-			// natomiast jeżeli mamy 1, ale przed mamy [ i za mamy ] to dajemy samo cytat,
-			// jeżeli mamy 1, ale przed nie mamy [ i za też nie mamy ] to znowu dajemy [cytat]
-			if(strpos($text, "[") !== false){ 
-				$xref->nodeValue = "[cytat]"; 
-			} else{							  
-				if(strpos($xref->getAttribute("rid"), "CIT") !== false){
-					$xref->nodeValue = "cytat";
-				}else{ 							
-					$xref->nodeValue = " [cytat]";
-				}		
-				
-			}
+			$xref->nodeValue = "[TU JEST CYTAT]";
 			echo $text."\r\n"; // pomocnicze wyświetlam cytaty
 		}
 		
@@ -57,11 +41,11 @@
 		echo $text; // podląd w przeglądarce zapisywanego tekstu
 		fwrite($myfile, $text);
 	}
-
 	// zamykamy plik w którym dokonaliśmy zapisu 
 	echo $counter; // pomocnicze wyświetlam liczbę cytatów
 	fclose($myfile);
 	// $dom->save("nowy.txt"); // jakbyśmy chcieli zapisać nową wersję xml 
+	}
 ?>
 </body>
 </html>
